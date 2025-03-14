a3755fadce6eaec1f1fb65fb9015ead73e0dfb7b
Esaius2058 <wabwirewild@gmail.com>
2025-03-14 22:30:57 +0300

    updated the controller functions to use the query functions from userQueries

--- packages/backend/src/controllers/userControllers.js
+++ packages/backend/src/controllers/userControllers.js
@@ -1,29 +1,19 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import prisma from "../utils/prisma/prisma";
+import { handleCreateUser, handleUpdateUser, handleDeleteUser,handleGetAllUsers, handleGetUser, handleGetUserByEmail } from "../config/queries/userQueries";
 
 export async function createUser(req, res) {
   try {
     const { username, email, password } = req.body;
 
-    const existingUser = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
+    const existingUser = await handleGetUserByEmail(email);
 
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await prisma.user.create({
-      data: {
-        username,
-        email,
-        password: hashedPassword,
-      },
-    });
+    const newUser = await handleCreateUser(email, name, hashedPassword);
 
     //generate JWT token
     const token = jwt.sign(
@@ -33,7 +23,7 @@ export async function createUser(req, res) {
     );
 
     res.status(201).json({ message: "User registered successfully",token });
-  } catch (errror) {
+  } catch (error) {
     console.error("Error creating user: ", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
@@ -43,11 +33,7 @@ export async function loginUser(req, res) {
     try{
         const { email, password } = req.body;
 
-        const user = await prisma.user.findUnique({
-            where: {
-                email,
-            },
-        });
+        const user = await handleGetUserByEmail(email);
 
         if (!user) {
             return res.status(400).json({ message: "Invalid credentials, no such user!" });
@@ -73,22 +59,72 @@ export async function loginUser(req, res) {
     }
 }
 
+export async function ensureAuthenticated(req, res, next) {
+    passport.authenticate("jwt", { session: false }, (err, user) => {
+        if (err || !user) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        req.user = user;
+        return next();
+    })(req, res, next);
+}
+
+export async function ensureAdmin(req, res, next) {
+    if (req.user.role !== "admin") {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    return next();
+}
+
+export async function getAllUsers(req, res) {
+    try {
+        const users = await handleGetAllUsers();
+
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Error fetching all users: ", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
 export async function getUserProfile(req, res) {
     try {
-        const user = await prisma.user.findUnique({
-            where: {
-                id: req.user.id,
-            },
-            select: {
-                id: true,
-                username: true,
-                email: true,
-            },
-        });
+      const userId = req.user.id;
+        const user = await handleGetUser(Number(userId));
 
         res.status(200).json(user);
     } catch (error) {
         console.error("Error fetching user profile: ", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+export async function updateUserProfile(req, res) {
+    try {
+        const userId = req.user.id;
+        const { username, email, password } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const updatedUser = await handleUpdateUser(userId, username, email, hashedPassword);
+
+        res.status(200).json({ message: "User updated successfully", updatedUser });
+    } catch (error) {
+        console.error("Error updating user profile: ", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+export async function deleteUserProfile(req, res) {
+    try {
+      const userId = req.user.id;
+        await handleDeleteUser(userId);
+
+        res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting user profile: ", error);
         res.status(500).json({ message: "Internal Server Error" });
     }
 }
