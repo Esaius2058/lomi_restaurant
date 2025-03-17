2b8e2701da0bfa6adbfb817797e8e79e487a417d
Esaius2058 <wabwirewild@gmail.com>
2025-03-17 19:51:45 +0300

    moved prisma.js to the config directory since it involves configuration

--- packages/backend/src/utils/prisma/prisma.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const globalForPrisma = globalThis;
-
-const prisma = globalForPrisma.prisma || new PrismaClient(); //  In production, it ensures only one instance is used.
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; // In development, it prevents new PrismaClient() from being created every time you save changes.
-
-export default prisma;
