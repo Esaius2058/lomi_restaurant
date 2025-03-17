e7f8af7a1d3c9a73e990a836cbed3bbf176b8540
Esaius2058 <wabwirewild@gmail.com>
2025-03-17 20:13:40 +0300

    updated the controller functions to use the query functionsfrom queries

--- packages/backend/src/controllers/orderControllers.js
+++ packages/backend/src/controllers/orderControllers.js
@@ -1,113 +1,90 @@
 import prisma from "../utils/prisma/prisma";
+import {
+  handleCreateOrder,
+  handleGetOrderById,
+  handleUpdateOrder,
+  handleDeleteOrder,
+  handleGetAllOrders,
+  handleGetOrderByStatus,
+} from "../config/queries/orderQueries";
 
 export async function createOrder(req, res) {
-    try {
-        const order = await prisma.order.create({
-            data: {
-                user: {
-                    connect: {
-                        id: req.user.id,
-                    },
-                },
-                food: {
-                    connect: {
-                        id: req.body.foodId,
-                    },
-                },
-            },
-        });
+  const userId = req.user.id;
+  const totalPrice = req.body.totalPrice;
+  try {
+    const order = await handleCreateOrder(userId, totalPrice);
 
-        res.status(201).json(order);
-    } catch (error) {
-        console.error("Error creating order: ", error);
-        res.status(500).json({ message: "Internal Server Error" });
-    }
+    res.status(201).json(order);
+  } catch (error) {
+    console.error("Error creating order: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 export async function getAllOrders(req, res) {
-    try {
-        const orders = await prisma.order.findMany({
-            where: {
-                userId: req.user.id,
-            },
-            include: {
-                food: true,
-            },
-        });
+  try {
+    const orders = await handleGetAllOrders();
 
-        res.status(200).json(orders);
-    } catch (error) {
-        console.error("Error fetching orders: ", error);
-        res.status(500).json({ message: "Internal Server Error" });
-    }
+    res.status(200).json(orders);
+  } catch (error) {
+    console.error("Error fetching orders: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 export async function getOrderById(req, res) {
-    try{
-        const order = await prisma.order.findUnique({
-            where: {
-                id: parseInt(req.params.id),
-            },
-        });
+  const userId = req.user.id;
+  const orderId = parseInt(req.params.id);
 
-        if(!order){
-            return res.status(404).json({ message: "Order not found" });
-        }       
+  try {
+    const order = await handleGetOrderById(orderId);
 
-        res.status(200).json(order);
-    }catch(error){
-        console.error("Error fetching order by id: ", error);
-        res.status(500).json({ message: "Internal Server Error" });1
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
     }
+
+    res.status(200).json(order);
+  } catch (error) {
+    console.error("Error fetching order by id: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+    1;
+  }
 }
 
 export async function updateOrder(req, res) {
-    try{
-        const order = await prisma.order.update({
-            where: {
-                id: parseInt(req.params.id),
-            },
-            data: {
-                status: req.body.status,
-            },
-        });
+  const orderId = parseInt(req.params.id);
+  const totalPrice = req.body.totalPrice;
+  try {
+    const order = await handleUpdateOrder(orderId, totalPrice);
 
-        res.status(200).json(order);
-    }catch(error){
-        console.error("Error updating order: ", error);
-        res.status(500).json({ message: "Internal Server Error" });
-    }
+    res.status(200).json(order);
+  } catch (error) {
+    console.error("Error updating order: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 export async function deleteOrder(req, res) {
-    try{
-        await prisma.order.delete({
-            where: {
-                id: parseInt(req.params.id),
-            },
-        });
+  const orderId = parseInt(req.params.id);
 
-        res.status(204).end();
-    }catch(error){
-        console.error("Error deleting order: ", error);
-        res.status(500).json({ message: "Internal Server Error" });
-    }
+  try {
+    await handleDeleteOrder(orderId);
+    res.status(204).end();
+  } catch (error) {
+    console.error("Error deleting order: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 export async function getOrdersByStatus(req, res) {
-    try{
-        const orders = await prisma.order.findMany({
-            where: {
-                status: req.params.status,
-            },
-            include: {
-                food: true,
-            },
-        });
+  const status = req.query.status;
 
-        res.status(200).json(orders);
-    }catch(error){
-        console.error("Error fetching orders by status: ", error);
-        res.status(500).json({ message: "Internal Server Error" });
-    }
+  try {
+    const orders = await handleGetOrderByStatus(status);
+
+    res.status(200).json(orders);
+  } catch (error) {
+    console.error("Error fetching orders by status: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
