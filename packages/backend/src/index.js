10c3d16ebb4d53333703d5e8f470621eb064615f
Esaius2058 <wabwirewild@gmail.com>
2025-04-16 13:23:43 +0300

    removed the functional and configuration code for the application
    this file now has one function,to run the server

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -1,31 +1,4 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import passport from "passport";
-import "./config/passport.js";
-import session from "express-session";
-import router from "./routes/lomiRoutes.js";
-
-dotenv.config(); // Load environment variables
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//Session middleware
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }, // Set to true if using https
-}));
-//Initialize Passport
-app.use(passport.initialize());
-// Use express-session middleware for passport
-app.use(passport.session());
-//Routes
-app.use("/api", router);
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
