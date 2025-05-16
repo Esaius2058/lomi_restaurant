91d7b0dd9f2fc66becd5a5c3c76ae83f4da66a91
Esaius2058 <wabwirewild@gmail.com>
2025-05-16 04:22:52 +0300

    initial commit

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router-dom";
+import { AuthProvider } from "./components/AuthContext";// Adjust path if needed
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>HELLO WORLD!!!</h1>
-    </>
-  )
+    <AuthProvider>
+      <div className="app-container">
+        <Outlet />
+      </div>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
