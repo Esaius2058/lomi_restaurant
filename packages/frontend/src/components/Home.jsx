77edcb27b8645db50d5965e3fac3b61f3b206246
VictorVikaki <vikakikimani@gmail.com>
2025-05-17 23:37:04 +0300

    Revert "feat(CTA): call to action button to order food"
    This reverts commit 024f331983fe2447fa2d4268810850bbfc7eb5de.

--- packages/frontend/src/components/Home.jsx
+++ packages/frontend/src/components/Home.jsx
@@ -4,23 +4,17 @@ const HomePage = () => {
   return (
     <div className="home-page">
       <NavBar />
-      <div className="home-intro-container">
-        <div className="home-intro">
-          <div className="home-intro-text">
-            <h1>Lomi Restaurant</h1>
-            <h2>Kula Local, Kula Fresh</h2>
-            <button>Order Now</button>
-          </div>
-          <div className="home-intro-image">
-            <img src="src/assets/food.png.jpg" alt="animation" />
-          </div>
+      <div className="home-intro">
+        <div className="home-intro-text">
+          <h1>Lomi Restaurant</h1>
+          <h2>Kula Local, Kula Fresh</h2>
         </div>
-      </div>
-      <div className="menu-preview">
-
+        <div className="home-intro-image">
+          <img src="" alt="animation" />
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default HomePage;
