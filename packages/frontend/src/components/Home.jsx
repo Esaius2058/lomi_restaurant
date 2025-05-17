0cf2dad3d9bbc82f7175a8168e549882b5a9d1a7
VictorVikaki <vikakikimani@gmail.com>
2025-05-17 23:42:04 +0300

    Resolve conflict in Home.jsx after stash pop

--- packages/frontend/src/components/Home.jsx
+++ packages/frontend/src/components/Home.jsx
@@ -4,13 +4,16 @@ const HomePage = () => {
   return (
     <div className="home-page">
       <NavBar />
-      <div className="home-intro">
-        <div className="home-intro-text">
-          <h1>Lomi Restaurant</h1>
-          <h2>Kula Local, Kula Fresh</h2>
-        </div>
-        <div className="home-intro-image">
-          <img src="" alt="animation" />
+      <div className="home-intro-container">
+        <div className="home-intro">
+          <div className="home-intro-text">
+            <h1>Lomi Restaurant</h1>
+            <h2>Kula Local, Kula Fresh</h2>
+            <button>Order Now</button>
+          </div>
+          <div className="home-intro-image">
+            <img src="src/assets/food.jpg" alt="animation" />
+          </div>
         </div>
       </div>
     </div>
