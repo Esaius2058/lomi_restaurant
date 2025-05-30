8281ee192d841bafcaf2c09860c36d8f21dbcfbc
Esaius2058 <wabwirewild@gmail.com>
2025-05-30 12:58:02 +0300

    feat(icons): import icons from lucide-react
    feat: created MenuNavbar for the menu page

--- packages/frontend/src/components/Navbar.jsx
+++ packages/frontend/src/components/Navbar.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
+import { ShoppingCartIcon, CircleUser } from "lucide-react";
 
 function NavBar() {
   return (
     <nav className="navbar">
       <div className="logo-container">
-        <Link to="/">
-          <img src={"/lomi_logo.svg"} alt={"lomi_logo"} className="lomi-logo" />
+        <Link to="/" className="lomi-logo">
+          <img
+            src={"/icons/lomi_logo.svg"}
+            alt={"lomi_logo"}
+          />
         </Link>
       </div>
       <div className="nav-links">
-        <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/about">About</Link>
         <Link to="/auth/signup">Sign Up</Link>
@@ -22,12 +25,14 @@ function AdminNavbar() {
   return (
     <nav className="navbar">
       <div className="logo-container">
-        <Link to="/">
-          <img src={"/lomi_logo.svg"} alt={"lomi_logo"} className="lomi-logo" />
+        <Link to="/" className="lomi-logo">
+          <img
+            src={"/icons/lomi_logo.svg"}
+            alt={"lomi_logo"}
+          />
         </Link>
       </div>
       <div className="nav-links">
-        <Link to="/">Home</Link>
         <Link to="">Menu</Link>
         <Link to="">About</Link>
         <Link to="">Sign Up</Link>
@@ -36,4 +41,27 @@ function AdminNavbar() {
   );
 }
 
-export { NavBar, AdminNavbar };
+function MenuNavbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo-container">
+        <Link to="/" className="lomi-logo">
+          <img
+            src={"/icons/lomi_logo.svg"}
+            alt={"lomi_logo"}
+          />
+        </Link>
+      </div>
+      <div className="nav-links">
+        <Link to="/menu/orders">
+          <ShoppingCartIcon size={30} />
+        </Link>
+        <Link to="">
+          <CircleUser size={30} />
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export { NavBar, AdminNavbar, MenuNavbar };
