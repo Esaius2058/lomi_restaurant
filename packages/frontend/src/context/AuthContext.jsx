dd490c86991c71a29d460f2624d33a171e795b59
Lee Kaburu <leekaburu@gmail.com>
2025-06-10 18:44:08 +0300

    Connected it to signup, login so as to create a global user

--- packages/frontend/src/context/AuthContext.jsx
+++ packages/frontend/src/context/AuthContext.jsx
@@ -1,18 +1,54 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { LoginApi, SignupApi, getCurrentUser } from "../services/auth";
 
 export const AuthContext = createContext(null);
 
-export const useAuth = () => useContext(AuthContext);
-
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const login = (userData) => setUser(userData);
-  const logout = () => setUser(null);
+  useEffect(() => {
+    async function loadUser() {
+      try {
+        const { success, user } = await getCurrentUser();
+        if (success) setUser(user);
+      } catch (error) {
+        console.error("Session load failed: ", error);
+      } finally {
+        setLoading(false);
+      } 
+    }
+    loadUser();
+  }, []);
+
+  const login = async (credentials) => {
+    const { success, user, message } = await LoginApi(credentials);
+    if (success) {
+      setUser(user);
+    };
+    return { success, message };
+  };
+
+  const signup = async (signUpData) => {
+    const { success, user, message } = await SignupApi(signUpData);
+    if (success) {
+      setUser(user);
+    };
+    return { success, message };
+  };
+
+  const logout = async () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, loading, login, signup, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
